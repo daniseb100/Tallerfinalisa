7df6f0f6be9723bbe0c56036491aeaac36ffb1c8
Elianna Cuasquer <elianacuasquer@gmail.com>
2022-10-09 19:58:57 +0100

    archivo 2

--- inicio/registrar/newproj.php
+++ inicio/registrar/newproj.php
@@ -1,7 +1,8 @@
 <?php
 include '../../config/conexion.php';
 $conexion = mysqli_connect($bd_servidor, $bd_usuario, $bd_clave, $bd_base);
-$sql = "INSERT INTO `tb_proyectos` (`id_proyect`,  `id_estudiante`,  `ruta`,  `nombre_file`,  `nom_proyecto`,  `modalidad`,  `fecha`,  `nota`, `observaciones`,`estado`)
+$date
+$sql = "INSERT INTO `tb_proyectos` ( `id_estudiante`,  `ruta`,  `nombre_file`,  `nom_proyecto`,  `modalidad`,  `fecha`,  `nota`, `observaciones`,`estado`)
         VALUES ('" . $_POST['txtApe1user'] . "', '" . $_POST['txtApe2user'] . "', '" . $_POST['txtNomb1user'] . "', '" . $_POST['txtNomb2user'] . "', '" . $_POST['mailuser'] . "', '" . $_POST['txtlogin'] . "', '" . $_POST['passu'] . "', '" . $_POST['slcRolUser'] . "')";
 echo mysqli_query($conexion, $sql);
 mysqli_close($conexion);
